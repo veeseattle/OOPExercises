345565fa67d72e8d8c690aecc572767c2536d4d7
veeseattle <v-vakurn@microsoft.com>
2016-03-14 14:39:03 -0700

    First commit - got Heron class and methods to work with int, need to get it working with doubles

---
+++ OOP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Heron
+    {
+        public int errorLimit;
+        public int value; 
+
+        public Heron (int errorLimitValue, int valueToCompute)
+        {
+            this.errorLimit = errorLimitValue;
+            this.value = valueToCompute;
+
+            if (this.value < 0)
+            {
+                throw new System.ArgumentException("Value to compute must be positive", "valueToCompute");
+            }         
+        }
+        public int ComputeSquareRoot()
+        {
+            int result = 0;
+            int guessedNumber = this.value / 2;
+            int guessedNumberSquared =  guessedNumber * guessedNumber;
+
+            while (Math.Abs(this.value - guessedNumberSquared) > this.errorLimit)
+            {
+                guessedNumber = (guessedNumber + this.value / guessedNumber) / 2;
+                guessedNumberSquared = guessedNumber * guessedNumber;
+            }
+            
+            result = guessedNumber;
+            return result;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Heron myNewHeron = new Heron(1, 64);
+            int myNewHeronSqrt = myNewHeron.ComputeSquareRoot();
+            Console.WriteLine("value of sqrt: {0}", myNewHeronSqrt);
+            Console.ReadLine();
+        }
+    }
+}
