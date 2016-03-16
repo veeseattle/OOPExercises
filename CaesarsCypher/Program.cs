aae4953856a9a4d0a075071378dac5dc1c5ecc5f
veeseattle <v-vakurn@microsoft.com>
2016-03-16 15:11:46 -0700

    Added comments

--- CaesarsCypher/Program.cs
+++ CaesarsCypher/Program.cs
@@ -11,12 +11,27 @@ namespace CaesarsCipher
         private string alphabet;
         private int offset;
 
+        /// <summary>
+        /// Constructor for CaesarsCipher that takes alphabet (string to be turned into char array)
+        /// and offset (in int). 
+        /// </summary>
+        /// <param name="alphabet"></param>
+        /// <param name="offset"></param>
         public CaesarsCipher(string alphabet, int offset)
         {
             this.alphabet = alphabet;
             this.offset = offset;
         }
         
+        /// <summary>
+        /// Encoding transforms the inputText (string) by transforming each character according to the 
+        /// offset and alphabet mapping provided in the CaesarsCipher constructor. It returns a transformed
+        /// string as an ouput. It assumes a circular alphabet and takes the mod of the alphabet length,
+        /// so if the index of a shifted letter is > the length of the alphabet, the index will wrap back 
+        /// to the beginning of the alphabet. 
+        /// </summary>
+        /// <param name="inputText"></param>
+        /// <returns></returns>
         public string Encode(string inputText)
         {
             var outputText = "";
@@ -37,6 +52,13 @@ namespace CaesarsCipher
             return outputText;
         }
         
+        /// <summary>
+        /// Decode reverses the transformation that Encode() does. It uses the same offset that was 
+        /// used to transformed the original string in Encode(), which means decoding an encoded 
+        /// string should return the original string. 
+        /// </summary>
+        /// <param name="inputText"></param>
+        /// <returns></returns>
         public string Decode(string inputText)
         {
             var outputText = "";
