41ef071a393930de48bdfb0a2635a9b4f5e50c7c
veeseattle <v-vakurn@microsoft.com>
2016-03-16 00:13:57 -0700

    added encode and decode

--- CaesarsCypher/Program.cs
+++ CaesarsCypher/Program.cs
@@ -6,93 +6,54 @@ using System.Threading.Tasks;
 
 namespace CaesarsCipher
 {
-
     class CaesarsCipher
     {
-        private string AlphabetString;
-        private Dictionary<char, char> Alphabet;
+        private string alphabet;
+        private int offset;
 
-        public CaesarsCipher (string inputAlphabet)
+        public CaesarsCipher(string alphabet, int offset)
         {
-            this.AlphabetString = inputAlphabet;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="inputText"></param>
-        /// <param name="offset"></param>
-        /// <returns></returns>
-        public void GenerateAlphabets(string inputText, int offset)
-        {
-            inputText = inputText.ToLower();
-
-            var cipher = new Dictionary<char, char>();
-            char[] charArray = inputText.ToCharArray();
-            for (int i = 0; i < charArray.Length; i++)
-            {
-                char letter = charArray[i];
-                int newIndex = i + offset;
-                if ( newIndex >= charArray.Length)
-                {
-                    newIndex = newIndex - charArray.Length;
-                }
-                if ( newIndex < 0)
-                {
-                    newIndex = newIndex + charArray.Length;
-                }
-                char newLetter = charArray[newIndex];
-                cipher[letter] = newLetter;
-            }
-            this.Alphabet = cipher;
-            //char[] buffer = inputText.ToCharArray();
-            //for (int i = 0; i < buffer.Length; i++)
-            //{
-            //    char letter = buffer[i];
-            //    if (letter > 'z')
-            //    {
-            //        letter = (char)(letter - 26);
-            //    }
-            //    if (letter < 'a')
-            //    {
-            //        letter = (char)(letter + 26);
-            //    }
-
-            //    letter = (char)(letter + offset);
-            //    buffer[i] = letter;
-            //}
-            //return buffer;
-
+            this.alphabet = alphabet;
+            this.offset = offset;
         }
         
-        public string ApplyCipher(string inputText)
+        public string Encode(string inputText)
         {
-            inputText = inputText.ToLower();
-            var newString = new char[inputText.Length];
-            var charArray = inputText.ToCharArray();
-            for (int i = 0; i < inputText.Length; i++)
+            var outputText = "";
+            char[] charArray = inputText.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
             {
-                var letter = charArray[i];
-                var newLetter = this.Alphabet[letter];
-
-                newString[i] = newLetter;
+                outputText += alphabet[(alphabet.IndexOf(charArray[i]) + offset) % alphabet.Length];
             }
-            var newNewString = newString.ToString();
 
-            return newNewString;
+            return outputText;
         }
-    }
 
-    class Program
-    {
-        static void Main(string[] args)
+        public string Decode(string inputText)
         {
-            var caesar = new CaesarsCipher(" ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-            var inputString = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            caesar.GenerateAlphabets(inputString, 3);
-            var newString = caesar.ApplyCipher("Hello Leo");
-            Console.WriteLine(newString);
-            Console.ReadLine();
+            var outputText = "";
+            char[] charArray = inputText.ToArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                outputText += alphabet[(alphabet.IndexOf(charArray[i]) - offset) % alphabet.Length];
+            }
+            return outputText;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var caesar = new CaesarsCipher(" ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3);
+
+                var output = caesar.Encode("HELLO LEO");
+                var decodedOutput = caesar.Decode(output);
+                Console.WriteLine(output);
+                Console.WriteLine(decodedOutput);
+                Console.ReadLine();
+            }
         }
     }
 }
